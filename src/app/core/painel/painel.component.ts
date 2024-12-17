02444fb4c687ebf4aa7fbecd044ee70361d5ef0d
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-12-16 22:21:18 -0300

    feat: tela de painel

--- src/app/core/painel/painel.component.ts
+++ src/app/core/painel/painel.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AcessoClientAuthenticatedUsecase } from '../acesso/services/acesso-client-authenticated.usecase';
 
 @Component({
   selector: 'app-painel',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './painel.component.html',
   styleUrl: './painel.component.scss'
 })
 export class PainelComponent {
-
+  get client() {
+    return this._client.clientAuthenticated();
+  }
+  
+  constructor(
+    private readonly _client: AcessoClientAuthenticatedUsecase
+  ) {
+    effect(() => {
+      console.log(this._client.clientAuthenticated());
+      
+    })
+  }
 }
