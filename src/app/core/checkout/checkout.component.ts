e0e8b5e1728f911f7db967c0b22456fac66d5cc0
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-12-18 16:57:41 -0300

    feat: tela de checkout

--- src/app/core/checkout/checkout.component.ts
+++ src/app/core/checkout/checkout.component.ts
@@ -3,19 +3,39 @@ import { Component, computed, OnDestroy, OnInit, signal } from "@angular/core";
 import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 import { ExcursoesSingleUsecase } from "../../features/pacotes/services/excursoes-single.usecase";
 import { CartItemComponent } from "../../shared/components/cart-item/cart-item.component";
+import { formatarData } from "../../shared/helpers/formatar-data.helper";
+import { EnumType } from "../../shared/models/global.type";
 import { AsyncLocalEmbarquePipe } from "../../shared/pipes/async-local-embarque.pipe";
 
 @Component({
   selector: "app-checkout",
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, AsyncLocalEmbarquePipe, CartItemComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AsyncLocalEmbarquePipe,
+    CartItemComponent,
+  ],
   templateUrl: "./checkout.component.html",
   styleUrl: "./checkout.component.scss",
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
-  public terms = new FormControl({ value: false, disabled: true }, Validators.required);
+  public terms = new FormControl(
+    { value: false, disabled: true },
+    Validators.required
+  );
   public pacote = signal<any>(null);
   public participantes = computed(() => this.pacote().participantes);
+  public totalValue = signal<number>(0);
+  public totalValueEmCentavos = computed(() => this.totalValue() * 100);
+  public amountTickets = computed(() => this.pacote().tickets.reduce((acc: number, item: EnumType<string>) => acc + item.value, 0));
+  public itensFree = computed(() => this.pacote().tickets.filter((item: EnumType<string>) => item.key === 'babies'));
+  public amountTicketsFree = computed(() => this.itensFree().reduce((acc: number, item: EnumType<string>) => acc + item.value, 0));
+  public itensPagos = computed(() => this.pacote().tickets.filter((item: EnumType<string>) => item.key !== 'babies'));
+  public amountTicketsPagos = computed(() => this.itensPagos().reduce((acc: number, item: EnumType<string>) => acc + item.value, 0));
+  public valorItensFree = computed(() => this.amountTicketsFree() * this.totalValueEmCentavos());
+  public valorItens = computed(() => this.totalValueEmCentavos() * this.amountTicketsPagos() );
+  public totalValueWithDiscount = computed(() => (this.valorItens() / this.amountTickets()));
 
   get excursao() {
     return this._service.excursao;
@@ -24,7 +44,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   get cantEnable() {
     return this.terms.value !== true;
   }
-  
+
   constructor(private readonly _service: ExcursoesSingleUsecase) {}
 
   public ngOnInit(): void {
@@ -37,17 +57,26 @@ export class CheckoutComponent implements OnInit, OnDestroy {
 
   onScroll(event: Event): void {
     const element = event.target as HTMLElement;
-  
+
     if (element) {
-      const atBottom = element.scrollTop + element.clientHeight >= element.scrollHeight;
-  
+      const atBottom =
+        element.scrollTop + element.clientHeight >= element.scrollHeight;
+
       // Atualiza o estado do botão baseado na rolagem
       if (atBottom) this.terms.enable();
     }
   }
 
   public adicionarAoCarrinho() {
+    let cart = JSON.parse(localStorage.getItem("cart")!) ?? [];
+    
+    cart.push(this.pacote());
 
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  private _formatandoPeriodo(dataInicio: string, dataFim: string) {
+    return `${formatarData(new Date(dataInicio))} a ${formatarData(new Date(dataFim))}`;
   }
 
   public ngOnDestroy(): void {
--- src/app/shared/components/cart-item/cart-item.component.ts
+++ src/app/shared/components/cart-item/cart-item.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, effect, EventEmitter, Input, Output, signal } from '@angular/core';
 import { Excursao, TipoPassageiroEnum, TipoPassageiroType } from '../../models/excursao.type';
 import { EnumType } from '../../models/global.type';
 
@@ -13,12 +13,19 @@ import { EnumType } from '../../models/global.type';
 export class CartItemComponent {
   public valor = computed(() => this.pacote().price);
   public tickets = computed(() => this.pacote().tickets.map((item: any) => ({ ...item, price: item.key === 'babies' ? 0 : this.valor() * item.value })));
-  public totalValue = computed(() => this.tickets().filter((item: any) => item.key !== 'babies').reduce((acc: number, item: EnumType<string>) => acc + item.value, 0) * this.valor());
   public totalValueWithDiscount = computed(() => this.totalValue() * 0.95);
+  public totalValue = computed(() => this.tickets().filter((item: any) => item.key !== 'babies').reduce((acc: number, item: EnumType<string>) => acc + item.value, 0) * this.valor());
   
   @Input({ required: true }) pacote = signal<any>(null);
   @Input({ required: true }) participantes = [];
   @Input({ required: true }) excursao: Excursao | null = null;
+
+  @Output() totalValueEmit = new EventEmitter<number>();
+
+  constructor() {
+    effect(() => this.totalValueEmit.emit(this.totalValue()))
+    effect(() => this.totalValueEmit.emit(this.totalValue()))
+  }
 
   public formatPassageiroTypeLabel(type: TipoPassageiroType) {
     return TipoPassageiroEnum[type];
