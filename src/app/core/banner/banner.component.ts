4fe9647f47f44d1c626e4358eca867f019efd601
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-12-22 15:31:29 -0300

    feat: integração do slide

--- src/app/core/banner/banner.component.ts
+++ src/app/core/banner/banner.component.ts
@@ -1,27 +1,22 @@
 import { CommonModule } from "@angular/common";
-import { Component, signal } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { BannerUsecase } from "./banner.usecase";
 
 @Component({
   selector: "app-banner",
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: "./banner.component.html",
-  styleUrl: "./banner.component.scss"
+  styleUrl: "./banner.component.scss",
 })
 export class BannerComponent {
-  // public slides = computed(() => this._usecase.slides().map((item: any) => item.configuracao))
-  slides = signal<any>([
-    { src: 'assets/images/fortaleza.webp', alt: 'Fortaleza' },
-    { src: 'assets/images/tiangua.webp', alt: 'Tianguá' },
-  ])
-  
+  public slides = computed(() => {
+    if (!this._usecase.slides()) return [];
+
+    return JSON.parse(this._usecase.slides().configuracao);
+  });
+
   constructor(private readonly _usecase: BannerUsecase) {
     this._usecase.carregarSlides();
-  }
-
-  public carregarBanners() {
   }
 }
