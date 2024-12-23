e7715a912765f22725e554808b5224460ff6e76c
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-12-23 09:57:47 -0300

    fix: insert whatsapp_link no env.prod

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,6 @@
 export const env = {
     API: 'https://8831-2804-29b8-516d-2314-c976-e831-afe5-d654.ngrok-free.app',
+    WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
     allowsUrls: [
         '/auth',
         'https://api.pagar.me/core/v5/paymentlinks'
