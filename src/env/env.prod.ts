ed556084ceafdb567615d15ae0e0d707a66413eb
Julio Magalhaes <julio.magalhaes@maps.com.br>
2025-01-27 09:40:05 -0300

    fix: ajuste env.api em produção

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,5 @@
 export const env = {
-    API: 'https://8831-2804-29b8-516d-2314-c976-e831-afe5-d654.ngrok-free.app',
+    API: 'http://localhost:8000', //'https://8831-2804-29b8-516d-2314-c976-e831-afe5-d654.ngrok-free.app',
     WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
     allowsUrls: [
         '/auth',
