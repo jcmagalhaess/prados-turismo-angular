4d945ec44057dd9d13f50f60e20b16becd2e8cd9
carlos Siqueira <carlooos.siqueira@gmail.com>
2025-01-30 11:11:21 -0300

    fix: download voucher client

--- src/app/core/pedidos/reserva.service.ts
+++ src/app/core/pedidos/reserva.service.ts
@@ -7,7 +7,7 @@ import { env } from "../../../env/env";
   providedIn: "root",
 })
 export class ReservaService {
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) { }
 
   public consultarReserva(id: string) {
     return lastValueFrom(this._http.get(`${env.API}/reserva/find/${id}`));
@@ -19,18 +19,19 @@ export class ReservaService {
     return lastValueFrom(
       this._http.get(`${env.API}/reserva/download-voucher-reserva/${id}`, { responseType: 'blob' })
     ).then(res => {
-      console.log(res);
-      
-      const url = window.URL.createObjectURL(new Blob([res], { type: 'application/pdf' }));
+      debugger
+
+      const url = window.URL.createObjectURL(new Blob([res], { type: 'text/csv' }));
       const link = document.createElement('a');
       link.href = url;
       link.download = 'voucher.pdf';
-      (link as any).body.appendChild(link);
+      document.body.appendChild(link);
       link.click();
 
       window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
       (link as any).body.removeChild(link);
     })
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
   }
 }
