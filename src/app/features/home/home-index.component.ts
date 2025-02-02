41b3676ad388585f6fb88580c4eeca0fbbb039be
Julio Magalhaes <julio.magalhaes@maps.com.br>
2025-02-02 13:03:23 -0300

    fix: removendo propriedade duplicada do filtro

--- src/app/features/home/home-index.component.ts
+++ src/app/features/home/home-index.component.ts
@@ -30,7 +30,7 @@ export class HomeIndexComponent {
   }
 
   constructor(private readonly _excursoes: ExcursoesListUsecase) {
-    this.getOrigem({ origem: '1', publicado: true });
+    this.getOrigem({ origem: '1' });
   }
 
   public getOrigem(filtro: any) {
