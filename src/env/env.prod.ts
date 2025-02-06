5c0527187054cec72d33eec34445455fd71aacde
carlos Siqueira <carlooos.siqueira@gmail.com>
2025-02-06 16:15:34 -0300

    testes 06022025

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,5 @@
 export const env = {
-    API: 'https://3a83-2804-29b8-516d-2b89-64bc-e06-1eff-fcab.ngrok-free.app',
+    API: 'https://34d9-2804-29b8-516d-2b89-f900-8d6c-13f6-25a8.ngrok-free.app',
     WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
     allowsUrls: [
         '/auth',
