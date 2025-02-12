e068148939d0f8961a38a2e7ac85ad050cb7c3cf
carlos Siqueira <carlooos.siqueira@gmail.com>
2025-02-12 15:59:14 -0300

    nova url producao

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,5 @@
 export const env = {
-    API: 'https://dbe6-2a02-4780-14-74e2-00-1.ngrok-free.app',
+    API: 'https://6dcb-2a02-4780-14-74e2-00-1.ngrok-free.app',
     WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
     allowsUrls: [
         '/auth',
