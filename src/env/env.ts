ce06a337b2e1199b95ba4a417f523b8eafe7d3da
Julio Magalhaes <julio.magalhaes@maps.com.br>
2025-02-19 16:53:20 -0300

    fix: ajuste no style do card

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    API: 'http://localhost:8000',
+    API: 'https://api.wiesestourismus.de',
     WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
     allowsUrls: [
         '/auth',
