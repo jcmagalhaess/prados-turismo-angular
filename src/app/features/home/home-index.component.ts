05b5688dc623209660a6b5a1b9253015347ad485
Julio Magalhaes <jcmagalhaes301@gmail.com>
2025-06-10 21:25:56 -0300

    fix: colocando parallax no banner

--- src/app/features/home/home-index.component.ts
+++ src/app/features/home/home-index.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnDestroy,
+} from "@angular/core";
 import { RouterModule } from "@angular/router";
+import LocomotiveScroll from "locomotive-scroll";
 import { BannerComponent } from "../../core/banner/banner.component";
 import { FeaturesComponent } from "../../core/features/features.component";
 import { HeaderStyleService } from "../../core/header/header-style.interceptor";
@@ -18,28 +24,54 @@ import { ExcursoesListUsecase } from "../pacotes/services/excursoes-list.usecase
     FeaturesComponent,
     ExcursaoCardComponent,
     PacotesFilterComponent,
-    RouterModule
+    RouterModule,
   ],
   templateUrl: "./home-index.component.html",
   styleUrl: "./home-index.component.scss",
-  providers: [ExcursoesListUsecase, HeaderStyleService]
+  providers: [ExcursoesListUsecase, HeaderStyleService],
 })
-export class HomeIndexComponent {
+export class HomeIndexComponent implements AfterViewInit, OnDestroy {
+  private scroll!: LocomotiveScroll;
+  backgroundPosition = 0;
   get excursoes() {
     return this._excursoes.excursoes;
   }
 
   constructor(private readonly _excursoes: ExcursoesListUsecase) {
-    this.getOrigem({ origem: '1' });
+    this.getOrigem({ origem: "1" });
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    this.backgroundPosition = scrollY * 0.4; // quanto menor o número, mais "delay"
   }
 
   public getOrigem(filtro: any) {
-    this._excursoes.getExcursoes({ ...filtro, orderBy: "dataInicio", publicado: true });
+    this._excursoes.getExcursoes({
+      ...filtro,
+      orderBy: "dataInicio",
+      publicado: true,
+    });
   }
 
   public hasUrl(image: ExcursaoImagem) {
     if (!!image) return image.url;
 
     return "";
+  }
+
+  ngAfterViewInit() {
+    // this.scroll = new LocomotiveScroll({
+    //   el: document.querySelector("[data-scroll-container]") as HTMLElement,
+    //   smooth: true,
+    //   lerp: 0.05, // menor = mais suave (0.05 a 0.1 costuma ser bom)
+    // });
+  }
+
+  ngOnDestroy() {
+    // if (this.scroll) {
+    //   this.scroll.destroy();
+    // }
   }
 }
--- src/app/shared/pipes/formata-parcelas.pipe.ts
+++ src/app/shared/pipes/formata-parcelas.pipe.ts
@@ -1,14 +1,12 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'formataParcelas',
-  standalone: true
+  name: "formataParcelas",
+  standalone: true,
 })
 export class FormataParcelasPipe implements PipeTransform {
-
-  transform(value: number): string {
-    if (value > 2000) return "em até 10x sem juros";
-    return 'em até 5x sem juros';
+  transform(value: string): string {
+    if (value.toLowerCase().includes("terrestre")) return "em até 5x sem juros";
+    return "em até 10x sem juros";
   }
-
 }
