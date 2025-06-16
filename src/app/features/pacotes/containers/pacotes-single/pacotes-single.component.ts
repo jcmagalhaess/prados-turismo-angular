fd351f64979058512759ac31849a7e5dfe19b8fd
Julio Magalhaes <jcmagalhaes301@gmail.com>
2025-06-16 10:28:42 -0300

    wip: alterando o single dos pacotes

--- src/app/features/pacotes/containers/pacotes-single/pacotes-single.component.ts
+++ src/app/features/pacotes/containers/pacotes-single/pacotes-single.component.ts
@@ -1,33 +1,33 @@
-import { CommonModule, CurrencyPipe } from '@angular/common';
-import { AfterViewInit, Component, computed, effect } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { Swiper } from 'swiper';
-import 'swiper/css'; // Importa estilos gerais
-import 'swiper/css/navigation'; // Estilos específicos para navegação
-import 'swiper/css/thumbs'; // Estilos para os thumbs
-import { Navigation, Thumbs } from 'swiper/modules';
-import { ToasterService } from '../../../../shared/components/toaster/toaster.service';
-import { formatarData } from '../../../../shared/helpers/formatar-data.helper';
-import { ExcursaoImagem } from '../../../../shared/models/excursao.type';
-import { PacotesSidebarComponent } from '../../components/pacotes-sidebar/pacotes-sidebar.component';
-import { ExcursoesSingleUsecase } from '../../services/excursoes-single.usecase';
+import { CommonModule, CurrencyPipe } from "@angular/common";
+import { AfterViewInit, Component, computed, effect } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { Swiper } from "swiper";
+import "swiper/css"; // Importa estilos gerais
+import "swiper/css/navigation"; // Estilos específicos para navegação
+import "swiper/css/thumbs"; // Estilos para os thumbs
+import { Navigation, Thumbs } from "swiper/modules";
+import { ToasterService } from "../../../../shared/components/toaster/toaster.service";
+import { formatarData } from "../../../../shared/helpers/formatar-data.helper";
+import { ExcursaoImagem } from "../../../../shared/models/excursao.type";
+import { PacotesSidebarComponent } from "../../components/pacotes-sidebar/pacotes-sidebar.component";
+import { ExcursoesSingleUsecase } from "../../services/excursoes-single.usecase";
 
 Swiper.use([Navigation, Thumbs]);
 
 @Component({
-  selector: 'app-pacotes-single',
+  selector: "app-pacotes-single",
   standalone: true,
   imports: [CommonModule, CurrencyPipe, PacotesSidebarComponent, RouterModule],
-  templateUrl: './pacotes-single.component.html',
-  styleUrl: './pacotes-single.component.scss',
-  providers: [ExcursoesSingleUsecase]
+  templateUrl: "./pacotes-single.component.html",
+  styleUrl: "./pacotes-single.component.scss",
+  providers: [ExcursoesSingleUsecase],
 })
 export class PacotesSingleComponent implements AfterViewInit {
   public form = new FormGroup({
-    period: new FormControl('', Validators.required),
-    tickets: new FormControl('', Validators.required),
-  })
+    period: new FormControl("", Validators.required),
+    tickets: new FormControl("", Validators.required),
+  });
   get excursao() {
     return this._service.excursao;
   }
@@ -37,46 +37,46 @@ export class PacotesSingleComponent implements AfterViewInit {
   }
 
   get opcionais() {
-    return computed(() => this.excursao()?.Pacotes.Produto)
+    return computed(() => this.excursao()?.Pacotes.Produto);
   }
 
   get noOpcionais() {
-    return computed(() => this.opcionais()?.length === 0)
+    return computed(() => this.opcionais()?.length === 0);
   }
 
   get isJeri() {
-    return computed(() => this.excursao()?.nome.includes('Jericoacoara') ?? false);
+    return computed(
+      () => this.excursao()?.nome.includes("Jericoacoara") ?? false
+    );
   }
-  
+
   constructor(
     private readonly _service: ExcursoesSingleUsecase,
     private readonly _activatedRoute: ActivatedRoute,
     private readonly _toaster: ToasterService
   ) {
-    this._service.getExcursaoById(this._activatedRoute.snapshot.params['id']);
+    this._service.getExcursaoById(this._activatedRoute.snapshot.params["id"]);
 
     effect(() => {
-      this.form.controls['period'].setValue(this.formatandoPeriodo(this.excursao()?.dataInicio!, this.excursao()?.dataFim!));
-    })
+      this.form.controls["period"].setValue(
+        this.formatandoPeriodo(
+          this.excursao()?.dataInicio!,
+          this.excursao()?.dataFim!
+        )
+      );
+    });
   }
 
   public ngAfterViewInit(): void {
-    const thumbsSwiper = new Swiper('.mySwiper', {
-      spaceBetween: 10,
-      slidesPerView: 4,
+    const mainSwiper = new Swiper(".mySwiper2", {
+      spaceBetween: 20,
+      slidesPerView: 3.5,
       freeMode: true,
+      height: 100,
       watchSlidesProgress: true,
-    });
-
-    // Inicialize o Swiper principal e conecte os thumbs
-    const mainSwiper = new Swiper('.mySwiper2', {
-      spaceBetween: 10,
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
-        swiper: thumbsSwiper, // Conecta os thumbs ao principal
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
   }
@@ -84,7 +84,7 @@ export class PacotesSingleComponent implements AfterViewInit {
   public hasUrl(image: ExcursaoImagem) {
     if (!!image) return image.url;
 
-    return '';
+    return "";
   }
 
   public formatGallery(images: ExcursaoImagem[]) {
@@ -96,56 +96,63 @@ export class PacotesSingleComponent implements AfterViewInit {
   }
 
   public formatandoDescricao(description: string) {
-    if (!description) return '';
-    
-    let splitDescription = description?.split('---');
+    if (!description) return "";
 
-    if (splitDescription[0].includes(';')) {
-        return '';
+    let splitDescription = description?.split("---");
+
+    if (splitDescription[0].includes(";")) {
+      return "";
     }
 
     return splitDescription[0];
   }
 
   public formatandoItensInclusos(description: string) {
-    if (!description) return '';
-    
-    let splitDescription = description.split('---');
+    if (!description) return "";
 
-    if (splitDescription[0].includes(';')) {
-        return this._buildItensInclusos(splitDescription[0].split(';'));
+    let splitDescription = description.split("---");
+
+    if (splitDescription[0].includes(";")) {
+      return this._buildItensInclusos(splitDescription[0].split(";"));
     }
 
-    return this._buildItensInclusos(splitDescription[1].split(';'));
+    return this._buildItensInclusos(splitDescription[1].split(";"));
   }
 
   public formatandoPeriodo(dataInicio: string, dataFim: string) {
-    return `${formatarData(new Date(dataInicio))} à ${formatarData(new Date(dataFim))}`;
+    return `${formatarData(new Date(dataInicio))} à ${formatarData(
+      new Date(dataFim)
+    )}`;
   }
-  
+
   private _buildItensInclusos(itens: Array<any>) {
-      let itensFilted = itens.filter(item => item !== '');
-  
-      return itensFilted;
+    let itensFilted = itens.filter((item) => item !== "");
+
+    return itensFilted;
   }
 
   public shareOnFacebook() {
     const encodedUrl = encodeURIComponent(window.location.href);
     const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
-    window.open(facebookUrl, '_blank');
-  }  
+    window.open(facebookUrl, "_blank");
+  }
 
   public shareMessageOnWhatsApp(message: string): void {
-    const encodedMessage = encodeURIComponent(message + ': ' + window.location.href);
+    const encodedMessage = encodeURIComponent(
+      message + ": " + window.location.href
+    );
     const url = `https://wa.me/?text=${encodedMessage}`;
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   }
 
   public clipboard() {
-    navigator.clipboard.writeText(window.location.href).then(() => {
-      this._toaster.success('Texto copiado para a área de transferência!');
-    }).catch(err => {
-      this._toaster.error(`Erro ao copiar: ${err}`);
-    });
+    navigator.clipboard
+      .writeText(window.location.href)
+      .then(() => {
+        this._toaster.success("Texto copiado para a área de transferência!");
+      })
+      .catch((err) => {
+        this._toaster.error(`Erro ao copiar: ${err}`);
+      });
   }
 }
--- src/env/env.ts
+++ src/env/env.ts
@@ -1,11 +1,13 @@
 export const env = {
-    API: 'http://localhost:8000',
-    WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
-    allowsUrls: [
-        '/auth',
-        'https://api.pagar.me/core/v5/paymentlinks',
-        'https://payment-link-sdx.pagar.me/api/admin'
-    ],
-    pagarmeApiUrl: 'https://payment-link-sdx.pagar.me/api/admin',
-    pagarmeApiKey: 'sk_test_53a0be2b782b4a70903117a71583d92d'
-}
+  API: "http://localhost:8000",
+  WHATSAPP_LINK:
+    "https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0",
+  allowsUrls: [
+    "/auth",
+    "/usuarios/login-user-client",
+    "https://api.pagar.me/core/v5/paymentlinks",
+    "https://payment-link-sdx.pagar.me/api/admin",
+  ],
+  pagarmeApiUrl: "https://payment-link-sdx.pagar.me/api/admin",
+  pagarmeApiKey: "sk_test_53a0be2b782b4a70903117a71583d92d",
+};
