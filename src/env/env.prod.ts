f7ea4ed97ab6f2d51a4650f95e56417290136311
Julio Magalhaes <jcmagalhaes301@gmail.com>
2025-06-24 20:05:31 -0300

    fix: configurando ngrok

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,10 +1,8 @@
 export const env = {
-    API: 'https://6dcb-2a02-4780-14-74e2-00-1.ngrok-free.app',
-    WHATSAPP_LINK: 'https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0',
-    allowsUrls: [
-        '/auth',
-        'https://api.pagar.me/core/v5/paymentlinks'
-    ],
-    pagarmeApiUrl: 'https://api.pagar.me/core/v5',
-    pagarmeApiKey: 'sk_65283e99e84e44598d1307ef859e8151'
-}
+  API: "https://4c93-201-51-132-68.ngrok-free.app",
+  WHATSAPP_LINK:
+    "https://api.whatsapp.com/send/?phone=5585997460786&text&type=phone_number&app_absent=0",
+  allowsUrls: ["/auth", "https://api.pagar.me/core/v5/paymentlinks"],
+  pagarmeApiUrl: "https://api.pagar.me/core/v5",
+  pagarmeApiKey: "sk_65283e99e84e44598d1307ef859e8151",
+};
