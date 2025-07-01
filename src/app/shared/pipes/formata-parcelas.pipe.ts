f37bfb9c96b89a06310e54d47193a087fa10f802
Julio Magalhaes <jcmagalhaes301@gmail.com>
2025-06-30 21:32:46 -0300

    fix: ajustando layout

--- src/app/shared/pipes/formata-parcelas.pipe.ts
+++ src/app/shared/pipes/formata-parcelas.pipe.ts
@@ -6,6 +6,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class FormataParcelasPipe implements PipeTransform {
   transform(value: string): string {
+    console.log(value);
+
     if (value.toLowerCase().includes("terrestre")) return "em até 5x sem juros";
     return "em até 10x sem juros";
   }
